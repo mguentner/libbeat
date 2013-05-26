02a9af062dba51d349d28de884d52d648b5a9844
Maximilian Güntner <maximilian.guentner@gmail.com>
2013-05-26 20:43:08 +0200

    compiles now with qt5
    Signed-off-by: Maximilian Güntner <maximilian.guentner@gmail.com>

--- SoundAnalyser/main.cpp
+++ SoundAnalyser/main.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <QtGui/QApplication>
+#include <QtWidgets/QApplication>
 #include "mainwindow.h"
 #include <iostream>
 int main(int argc, char *argv[])
